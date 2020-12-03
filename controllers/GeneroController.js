4334f43111bfb6a07cce078fc8e4b1a7f5b6cec7
rafaoctavio <rafaoctavio@gmail.com>
2020-12-03 12:15:43 -0300

    corrigindo pequenos detalhes no codigo (comentarios desnecessarios)

--- controllers/GeneroController.js
+++ controllers/GeneroController.js
@@ -2,9 +2,6 @@ const { Genero } = require('../models')
 
 var GeneroController = {
     home: async (req, res) => {
-        // Listar os produtos
-        // Receber os dados do banco
-        // Chamar a view e passar os dados que vieram do banco
         const result = await Genero.findAll();
         res.render('genero/home', {result});
     },
